b4645c8186f465c08b395500043a85df02edad3a
taylore1-99 <taylore1@findlay.edu>
2022-03-08 10:16:19 -0500

    Added Items to database class

--- ProMedica Innovations/ProMedica Innovations/Models/Item.cs
+++ ProMedica Innovations/ProMedica Innovations/Models/Item.cs
@@ -2,10 +2,20 @@
 
 namespace ProMedica_Innovations.Models
 {
-    public class Item
+    public class Idea
     {
-        public string Id { get; set; }
-        public string Text { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
         public string Description { get; set; }
+
+        public string Availability { get; set; }
+
+        public string Referral { get; set; }
+
+        public bool Employee { get; set; }
     }
 }
---
+++ ProMedica Innovations/ProMedica Innovations/Views/SplashScreen.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProMedica_Innovations.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SplashScreen : ContentPage
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
