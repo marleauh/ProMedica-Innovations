08ab1b8aa95bbae24add23f2cfdd4e7973703a37
Hunter J Marleau <marleauh@findlay.edu>
2022-03-10 10:29:29 -0500

    Replaced "Item" with "Idea"

--- ProMedica Innovations/ProMedica Innovations/Services/IDataStore.cs
+++ ProMedica Innovations/ProMedica Innovations/Services/IDataStore.cs
@@ -6,10 +6,10 @@ namespace ProMedica_Innovations.Services
 {
     public interface IDataStore<T>
     {
-        Task<bool> AddItemAsync(T Idea);
-        Task<bool> UpdateItemAsync(T Idea);
-        Task<bool> DeleteItemAsync(string Id);
-        Task<T> GetItemAsync(string Id);
-        Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false);
+        Task<bool> AddIdeaAsync(T Idea);
+        Task<bool> UpdateIdeaAsync(T Idea);
+        Task<bool> DeleteIdeaAsync(string Id);
+        Task<T> GetIdeaAsync(string Id);
+        Task<IEnumerable<T>> GetIdeasAsync(bool forceRefresh = false);
     }
 }
--- ProMedica Innovations/ProMedica Innovations/Services/MockDataStore.cs
+++ ProMedica Innovations/ProMedica Innovations/Services/MockDataStore.cs
@@ -6,55 +6,51 @@ using System.Threading.Tasks;
 
 namespace ProMedica_Innovations.Services
 {
-    public class MockDataStore : IDataStore<Item>
+    public class MockDataStore : IDataStore<Idea>
     {
-        readonly List<Item> items;
+        readonly List<Idea> ideas;
 
         public MockDataStore()
         {
-            items = new List<Item>()
+            ideas = new List<Idea>()
             {
-                new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
+                new Idea { Id = Guid.NewGuid().ToString(), FirstName = "Hunter", LastName = "Marleau", Email = "marleauh@findlay.edu", Phone = "4196019566", Description = "Test input", Availability = "Now", Referral = "Espyn", Employee = false},
+                //new Idea { Id = Guid.NewGuid().ToString(), FirstName = "Hunter", LastName = "Marleau", Email = "marleauh@findlay.edu", Phone = "4196019566", Description = "Test input", Availability = "Now", Referral = "Espyn", Employee = false},
             };
         }
 
-        public async Task<bool> AddItemAsync(Item item)
+        public async Task<bool> AddIdeaAsync(Idea idea)
         {
-            items.Add(item);
+            ideas.Add(idea);
 
             return await Task.FromResult(true);
         }
 
-        public async Task<bool> UpdateItemAsync(Item item)
+        public async Task<bool> UpdateIdeaAsync(Idea idea)
         {
-            var oldItem = items.Where((Item arg) => arg.Id == item.Id).FirstOrDefault();
-            items.Remove(oldItem);
-            items.Add(item);
+            var oldIdea = ideas.Where((Idea arg) => arg.Id == idea.Id).FirstOrDefault();
+            ideas.Remove(oldIdea);
+            ideas.Add(idea);
 
             return await Task.FromResult(true);
         }
 
-        public async Task<bool> DeleteItemAsync(string id)
+        public async Task<bool> DeleteIdeaAsync(string id)
         {
-            var oldItem = items.Where((Item arg) => arg.Id == id).FirstOrDefault();
-            items.Remove(oldItem);
+            var oldIdea = ideas.Where((Idea arg) => arg.Id == id).FirstOrDefault();
+            ideas.Remove(oldIdea);
 
             return await Task.FromResult(true);
         }
 
-        public async Task<Item> GetItemAsync(string id)
+        public async Task<Idea> GetIdeaAsync(string id)
         {
-            return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
+            return await Task.FromResult(ideas.FirstOrDefault(s => s.Id == id));
         }
 
-        public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
+        public async Task<IEnumerable<Idea>> GetIdeasAsync(bool forceRefresh = false)
         {
-            return await Task.FromResult(items);
+            return await Task.FromResult(ideas);
         }
     }
 }
