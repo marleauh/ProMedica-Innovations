21cf28deb8309c857b663c2cb3d5b081a89c168f
taylore1-99 <taylore1@findlay.edu>
2022-03-17 10:14:45 -0400

    Revert "Finally got the navigation to work!!"
    This reverts commit 8a65a19d4e0a670313de8a33645bd94ccda5e8f2.

--- ProMedica Innovations/ProMedica Innovations/Views/AboutPage.xaml.cs
+++ ProMedica Innovations/ProMedica Innovations/Views/AboutPage.xaml.cs
@@ -13,8 +13,7 @@ namespace ProMedica_Innovations.Views
         }
         private async void OnButtonClicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new NewIdeaPage()); //This navigates to the Submit Idea page when the "Submit Idea" button is clicked
-                                                         //We can make this work with other pages when we make them if needed
+            await Shell.Current.GoToAsync("//IdeasPage");
         }
     }
 }
