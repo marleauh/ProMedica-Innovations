35e840d277a84472c2e6393e1a6ede592b9c5585
Hunter J Marleau <marleauh@findlay.edu>
2022-03-31 09:45:17 -0400

    Updated Home Screen Tile Redirects and commands in view model

--- ProMedica Innovations/ProMedica Innovations/ViewModels/SplashScreenViewModel.cs
+++ ProMedica Innovations/ProMedica Innovations/ViewModels/SplashScreenViewModel.cs
@@ -11,11 +11,21 @@ namespace ProMedica_Innovations.ViewModels
     {
         public ICommand AboutCommand { private set; get; }
         public ICommand SubmitCommand { private set; get; }
+        public ICommand InventorsCommand { private set; get; }
+        public ICommand TechnologyCommand { private set; get; }
+        public ICommand VenturesCommand { private set; get; }
+        public ICommand DevelopmentCommand { private set; get; }
+        public ICommand PITeamCommand { private set; get; }
 
         public SplashScreenViewModel()
         {
             AboutCommand = new Command(OnAboutButtonClicked);
             SubmitCommand = new Command(OnSubmitButtonClicked);
+            InventorsCommand = new Command(OnInventorsButtonClicked);
+            TechnologyCommand = new Command(OnTechnologyButtonClicked);
+            VenturesCommand = new Command(OnVenturesButtonClicked);
+            DevelopmentCommand = new Command(OnDevelopmentButtonClicked);
+            PITeamCommand = new Command(OnPITeamButtonClicked);
         }
 
         private async void OnAboutButtonClicked(object sender)
@@ -26,6 +36,26 @@ namespace ProMedica_Innovations.ViewModels
         private async void OnSubmitButtonClicked(object sender)
         {
             await Shell.Current.GoToAsync("//TermsOfServicePage"); //Goes immediately to the terms of service page
+        }
+        private async void OnInventorsButtonClicked(object sender)
+        {
+            await Shell.Current.GoToAsync("//InventorsPage");
+        }
+        private async void OnTechnologyButtonClicked(object sender)
+        {
+            await Shell.Current.GoToAsync("//TechnologyPage");
+        }
+        private async void OnVenturesButtonClicked(object sender)
+        {
+            await Shell.Current.GoToAsync("//VenturesPage");
+        }
+        private async void OnDevelopmentButtonClicked(object sender)
+        {
+            await Shell.Current.GoToAsync("//DevelopmentPage");
+        }
+        private async void OnPITeamButtonClicked(object sender)
+        {
+            await Shell.Current.GoToAsync("//DevelopmentPage");
         }
     }
 }
