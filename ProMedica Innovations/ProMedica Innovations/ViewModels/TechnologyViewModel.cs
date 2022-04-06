49282d0f210f7e4e0a375d189c9d970544b866d2
Espyn <taylore1@findlay.edu>
2022-04-06 11:27:19 -0400

    Buttons now work. Pdf is downloaded to the device. Not sure how to open up in a webview

---
+++ ProMedica Innovations/ProMedica Innovations/ViewModels/TechnologyViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ProMedica_Innovations.ViewModels
+{
+    public class TechnologyViewModel : BaseViewModel
+    {
+        public ICommand OpenFlexScore { get; }
+        public ICommand OpenLacs { get; }
+
+        public ICommand OpenPdf { get; }
+
+        public TechnologyViewModel()
+        {
+            OpenFlexScore = new Command(async () => await Browser.OpenAsync("https://www.venturemedgroup.com/"));
+            OpenLacs = new Command(async () => await Browser.OpenAsync("https://www.kapioshealth.com/laboratoryassuredcompliancesolution.html"));
+            OpenPdf = new Command(async () => await Browser.OpenAsync("https://www.promedica.org/innovations/_assets/documents/style-library/snippets/radialcheckdevice.pdf"));
+        }
+
+    }
+}
--- ProMedica Innovations/ProMedica Innovations/Views/DevelopmentPage.xaml.cs
+++ ProMedica Innovations/ProMedica Innovations/Views/DevelopmentPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProMedica_Innovations.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace ProMedica_Innovations.Views
         public DevelopmentPage()
         {
             InitializeComponent();
+            BindingContext = new DevelopmentViewModel();
         }
     }
 }
