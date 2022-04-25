b18fbcd90337aa83f412956f805eaa1f92bd72a1
Espyn <taylore1@findlay.edu>
2022-04-25 16:01:35 -0400

    Fixed application break on news page. buttons on venture page not working

--- ProMedica Innovations/ProMedica Innovations/ViewModels/VentureViewModel.cs
+++ ProMedica Innovations/ProMedica Innovations/ViewModels/VentureViewModel.cs
@@ -7,14 +7,16 @@ namespace ProMedica_Innovations.ViewModels
 {
     public class VentureViewModel : BaseViewModel
     {
+        public ICommand OpenFund { get; }
+        public ICommand OpenNWFund { get; }
         public VentureViewModel()
         {
             OpenFund = new Command(async () => await Browser.OpenAsync("https://www.promedica.org/innovations/ventures/#venturefund-nav"));
             // command used by button to redirect to ProMedica's Venture Fund website
-
+            OpenNWFund = new Command(async () => await Browser.OpenAsync("https://www.promedica.org/innovations/ventures/#ohiotech-nav"));
         }
 
-        public ICommand OpenFund { get; }
+
 
     }
 }
