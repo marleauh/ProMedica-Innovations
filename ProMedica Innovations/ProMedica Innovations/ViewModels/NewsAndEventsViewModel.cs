e81225dc29791036955437d397f1f8e18572e653
katie <trauschk@findlay.edu>
2022-04-25 00:08:17 -0400

    Add social media icons to news page

--- ProMedica Innovations/ProMedica Innovations/ViewModels/NewsAndEventsViewModel.cs
+++ ProMedica Innovations/ProMedica Innovations/ViewModels/NewsAndEventsViewModel.cs
@@ -17,11 +17,15 @@ namespace ProMedica_Innovations.ViewModels
 
             OpenArticle3 = new Command(async () => await Browser.OpenAsync("https://hips.healthcare/member/john-p-pigott"));
             // command used by button to redirect to ProMedica's PI Officer Article
+
+            OpenMaps = new Command(async () => await Browser.OpenAsync("https://www.bing.com/maps?osid=95e1277c-6e9d-47c5-bbd6-608be4fc35c5&cp=41.673405~-83.676729&lvl=16&imgid=8d190d09-75e0-49c1-8a9f-31595803dedf&v=2&sV=2&form=S00027"));
+            // command used by button to redirect to ProMedica's Google Listing and Maps Location
         }
 
         public ICommand OpenNextBigIdea { get; }
         public ICommand OpenArticle2 { get; }
         public ICommand OpenArticle3 { get; }
+        public ICommand OpenMaps { get; }
 
     }
 }
