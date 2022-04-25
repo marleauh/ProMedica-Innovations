723772d2a2c2a3d86fa5a9bff67589527ebb36f3
katie <trauschk@findlay.edu>
2022-04-24 20:06:11 -0400

    News and Events Page Articles

--- ProMedica Innovations/ProMedica Innovations/ViewModels/NewsAndEventsViewModel.cs
+++ ProMedica Innovations/ProMedica Innovations/ViewModels/NewsAndEventsViewModel.cs
@@ -9,11 +9,19 @@ namespace ProMedica_Innovations.ViewModels
     {
         public NewsAndEventsViewModel()
         {
-            Title = "Read More";
             OpenNextBigIdea = new Command(async () => await Browser.OpenAsync("https://nextbigidea.health/"));
             // command used by button to redirect to ProMedica's Next Big Idea Contest website
+
+            OpenArticle2 = new Command(async () => await Browser.OpenAsync("https://www.prnewswire.com/news-releases/tela-bio-announces-investment-agreement-with-promedica-innovations-venture-fund-300619295.html?tc=eml_cleartime"));
+            // command used by button to redirect to ProMedica's Venture Article
+
+            OpenArticle3 = new Command(async () => await Browser.OpenAsync("https://hips.healthcare/member/john-p-pigott"));
+            // command used by button to redirect to ProMedica's PI Officer Article
         }
 
         public ICommand OpenNextBigIdea { get; }
+        public ICommand OpenArticle2 { get; }
+        public ICommand OpenArticle3 { get; }
+
     }
 }
