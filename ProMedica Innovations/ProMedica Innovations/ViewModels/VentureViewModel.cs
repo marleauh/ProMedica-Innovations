e3420c87587cb8925467e115c0029129db67f81d
katie <trauschk@findlay.edu>
2022-04-25 04:53:23 -0400

    fixed formatting/fonts/colors in inventor, news, technology, venture

---
+++ ProMedica Innovations/ProMedica Innovations/ViewModels/VentureViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ProMedica_Innovations.ViewModels
+{
+    public class VentureViewModel : BaseViewModel
+    {
+        public VentureViewModel()
+        {
+            OpenFund = new Command(async () => await Browser.OpenAsync("https://www.promedica.org/innovations/ventures/#venturefund-nav"));
+            // command used by button to redirect to ProMedica's Venture Fund website
+
+        }
+
+        public ICommand OpenFund { get; }
+
+    }
+}
