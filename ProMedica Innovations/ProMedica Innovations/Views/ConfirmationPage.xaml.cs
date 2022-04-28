fb30a67516d47fb40383163447d505be5105552d
taylore1-99 <taylore1@findlay.edu>
2022-04-28 10:34:47 -0400

    NAvigation for confirmation page needs work

---
+++ ProMedica Innovations/ProMedica Innovations/Views/ConfirmationPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProMedica_Innovations.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConfirmationPage : ContentPage
+    {
+        public ConfirmationPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ContinueBtn_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//SplashScreen");
+        }
+    }
+}
